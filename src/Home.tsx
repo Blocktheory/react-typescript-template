c0c1f894c4a726d037c89efd8079f4edb1b36ad6
Srivathsa-km <srivathsa@designstring.com>
2022-09-12 10:55:00 +0530

    classes added for segment

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,23 @@
-import { clickEvent } from "./Utils";
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+//import { clickEvent } from "./Utils";
+import { SegmentAnalytics } from "./Utils";
 
 const Home = () => {
   const trackButton = () => {
-    clickEvent("Article Bookmarked", "click", {
+    // clickEvent("Article Bookmarked", "click", {
+    //   title: "Snow Fall",
+    //   subtitle: "The Avalanche at Tunnel Creek",
+    //   author: "John Branch",
+    // });
+    new SegmentAnalytics("Article Bookmarked", "click", {
       title: "Snow Fall",
       subtitle: "The Avalanche at Tunnel Creek",
       author: "John Branch",
-    });
+    })?.event;
+
+    new SegmentAnalytics("UserId", "indentify", "srivathsa", "srivathsakm@gmail.com")?.event;
   };
+
   return (
     <div>
       <button onClick={() => trackButton()}>Click Here</button>
--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,17 +1,56 @@
+/* eslint-disable getter-return */
 import { AnalyticsBrowser } from "@segment/analytics-next";
 
 export const analytics = AnalyticsBrowser.load({ writeKey: "WyeQ5YVlqPdhnZijQrDtxzJKzFZ6IgCl" });
 
-export const clickEvent = (trackHeader: string, name: string, props: any) => {
-  window.analytics?.track(trackHeader, {
-    eventName: name,
-    ...props,
-  });
-};
+// export const clickEvent = (eventHeader: string, name: string, props: any) => {
+//   window.analytics?.track(eventHeader, {
+//     eventName: name,
+//     ...props,
+//   });
+// };
 
-export const setIdentity = (userId: string, name: string, email: string) => {
-  window.analytics?.identify(userId, {
-    name: name,
-    email: email,
-  });
-};
+// export const setIdentity = (userId: string, name: string, email: string) => {
+//   window.analytics?.identify(userId, {
+//     name: name,
+//     email: email,
+//   });
+// };
+
+export class SegmentAnalytics {
+  eventName: string;
+  eventHeader?: string | null;
+  props?: any;
+  name?: string | null;
+  email?: string | null;
+
+  constructor(eventHeader: string, eventName: string, props?: any, name?: string, email?: string) {
+    this.eventHeader = eventHeader;
+    this.eventName = eventName;
+    this.props = props;
+    this.email = email;
+    this.name = name;
+  }
+
+  get event() {
+    if (this.eventName === "click") {
+      return this.clickEvent();
+    } else if (this.eventName === "indentify") {
+      return this.setIndentify();
+    }
+  }
+
+  clickEvent() {
+    return window.analytics?.track(this.eventHeader as string, {
+      eventName: this.eventName,
+      ...this.props,
+    });
+  }
+
+  setIndentify() {
+    return window.analytics?.identify(this.eventHeader as string, {
+      name: this.name,
+      email: this.email,
+    });
+  }
+}
